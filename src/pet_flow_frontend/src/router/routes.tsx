7f0d6331551c64e0ddaf0f4a93894b38ce75689f
juliamascari <juliamascari24@gmail.com>
2026-05-09 15:22:37 -0300

    feat: cria página de serviços

--- src/pet_flow_frontend/src/router/routes.tsx
+++ src/pet_flow_frontend/src/router/routes.tsx
@@ -8,6 +8,7 @@ import Dashboard from '../pages/Dashboard/Dashboard';
 import Financial from '../pages/Financial/Financial';
 import Scheduling from '../pages/Scheduling/Scheduling';
 import Employees from '../pages/Employees/Employees';
+import Services from '../pages/Services/Services';
 import Placeholder from '../pages/Placeholder/Placeholder';
 import Pets from '../pages/Pets/Pets';
 import Clinica from '../pages/Clinica/clinica'; 
@@ -36,7 +37,7 @@ export default function AppRoutes() {
         <Route path="/agendamentos" element={<Scheduling />} />
         <Route path="/pets" element={<Pets />} />
         <Route path="/tutores" element={<Placeholder title="Tutores" />} />
-        <Route path="/servicos" element={<Placeholder title="Serviços" />} />
+        <Route path="/servicos" element={<Services />} />
         <Route path="/produtos" element={<Placeholder title="Produtos" />} />
         <Route path="/financeiro" element={<Financial />} />
         <Route path="/funcionarios" element={<Employees />} />
