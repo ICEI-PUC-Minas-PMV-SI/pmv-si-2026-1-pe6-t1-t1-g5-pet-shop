c308f727037dc248d3819f26e29a2b442e351016
Vinicius Neves <vinicius.neeves@outlook.com>
2026-05-09 14:14:03 -0300

    fix: format vaccine service files

--- src/pet_flow_backend/services/vaccine/vaccine.routes.ts
+++ src/pet_flow_backend/services/vaccine/vaccine.routes.ts
@@ -10,7 +10,9 @@ export class VaccineRoutes {
 
   private initRoutes(): void {
     this.router.get("/", (req, res) => this.controller.list(req, res));
-    this.router.get("/pet/:petId", (req, res) => this.controller.listByPet(req, res));
+    this.router.get("/pet/:petId", (req, res) =>
+      this.controller.listByPet(req, res),
+    );
     this.router.get("/:id", (req, res) => this.controller.getById(req, res));
     this.router.post("/", (req, res) => this.controller.create(req, res));
     this.router.put("/:id", (req, res) => this.controller.update(req, res));
